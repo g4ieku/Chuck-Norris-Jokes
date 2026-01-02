ad29133545ad5f191d865f5efabad30458b196b1
g4ieku <121449657+g4ieku@users.noreply.github.com>
2026-01-02 22:13:16 +0100

    update the error handling

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const jokeBtn = document.querySelector(".btn");
+const jokeTxt = document.querySelector("#joke_txt");
 
 const loadJoke = async () => {
   try {
     const response = await fetch("https://api.chucknorris.io/jokes/random");
-    if (!response.ok) throw new error("Cannot fetch the data");
+    if (!response.ok) throw new Error("Cannot fetch the data");
 
-    const data = await response.json();
-    return data;
+    return await response.json();
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    jokeTxt.textContent = "Cannot fetch the data";
+    return null; // return null so createJoke knows it failed
   }
 };
 
 const createJoke = async () => {
   const joke = await loadJoke();
 
-  document.querySelector("#joke_txt").textContent = joke["value"];
+  if (joke) jokeTxt.textContent = joke["value"];
 };
 
 jokeBtn.addEventListener("click", createJoke);
